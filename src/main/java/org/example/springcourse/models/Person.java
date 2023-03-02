9c225a39ec8f2e917e379eb988a3d790fe36572c
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-03-02 17:36:40 +0400

    crud. patch delete

--- src/main/java/org/example/springcourse/models/Person.java
+++ src/main/java/org/example/springcourse/models/Person.java
@@ -6,10 +6,14 @@ package org.example.springcourse.models;
 public class Person {
     private int id;
     private String name;
+    private int age;
+    private String email;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public Person() {
@@ -30,5 +34,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
